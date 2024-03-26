6ef45ed73250d6adffe3b527f59d0f75e4f1dfc2
Vankad24 <vankad24@mail.ru>
2024-03-26 16:12:20 +0800

    Delete and rename folders

--- src-simple/token.java
+++
@@ -1,8 +0,0 @@
-public class token {
-    public int cat;
-    public String text;
-    public int lineno;
-    public token(int c, String s, int l) {
-        cat = c; text = s; lineno = l;
-    }
-}
--- texts/hello.java
+++
@@ -1,6 +0,0 @@
-public class ClassHello {
-   
-   public static void main(String argv[]) {
-      System.out.println("hello, jzero!");
-   }
-}
